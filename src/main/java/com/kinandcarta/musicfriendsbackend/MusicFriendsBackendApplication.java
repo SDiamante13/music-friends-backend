30f8f53a0eecbe2c5c860c55c509a40453003870
Steven Diamante <stevendiamante13@gmail.com>
2022-01-07 17:07:08 -0500

    Initial commit.

---
+++ src/main/java/com/kinandcarta/musicfriendsbackend/MusicFriendsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kinandcarta.musicfriendsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicFriendsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicFriendsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinandcarta/musicfriendsbackend/MusicFriendsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinandcarta.musicfriendsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicFriendsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
